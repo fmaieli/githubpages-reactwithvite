bbe6c0265a93c692ee750c770bff19d9832a9e59
fmaieli <maieli.franco@gmail.com>
2023-05-16 18:30:41 -0300

    another change to test Github Actions 6

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Creating a h1 to test Github Actions 5</h1>
+      <h1>Creating a h1 to test Github Actions 6</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
